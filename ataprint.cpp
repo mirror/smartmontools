mirror/smartmontools

commit 8e96429f56176d761028715ee00948732fbe1f3c

smartctl: Print help message if no option is specified (ticket #39). Don't issue any other ATA command if only '-n POWERMODE' is specified.
git-svn-id: https://svn.code.sf.net/p/smartmontools/code/trunk/smartmontools@3249 4ea69e1a-61f1-4043-bf83-b5c94c648137

ataprint.cpp
@@ -1761,8 +1761,6 @@ static void ataPrintSCTErrorRecoveryControl(unsigned short read_timer, unsigned 
 
 int ataPrintMain (ata_device * device, const ata_print_options & options)
 {
-  int returnval = 0;
-
   // If requested, check power mode first
   const char * powername = 0;
   bool powerchg = false;
@@ -1772,7 +1770,7 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
     switch (powermode) {
       case -1:
         if (errno == ENOSYS) {
-          pout("CHECK POWER STATUS not implemented, ignoring -n Option\n"); break;
+          pout("CHECK POWER MODE not implemented, ignoring -n option\n"); break;
         }
         powername = "SLEEP";   powerlimit = 2;
         break;
@@ -1783,7 +1781,7 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
       case 0xff:
         powername = "ACTIVE or IDLE"; break;
       default:
-        pout("CHECK POWER STATUS returned %d, not ATA compliant, ignoring -n Option\n", powermode);
+        pout("CHECK POWER MODE returned unknown value 0x%02x, ignoring -n option\n", powermode);
         break;
     }
     if (powername) {
@@ -1824,7 +1822,47 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
        || options.smart_disable
   );
 
+  // SMART and GP log directories needed ?
+  bool need_smart_logdir = options.smart_logdir;
+
+  bool need_gp_logdir  = (
+          options.gp_logdir
+       || options.smart_ext_error_log
+       || options.smart_ext_selftest_log
+       || options.sataphy
+  );
+
+  unsigned i;
+  for (i = 0; i < options.log_requests.size(); i++) {
+    if (options.log_requests[i].gpl)
+      need_gp_logdir = true;
+    else
+      need_smart_logdir = true;
+  }
+
+  // SCT commands needed ?
+  bool need_sct_support = (
+          options.sct_temp_sts
+       || options.sct_temp_hist
+       || options.sct_temp_int
+       || options.sct_erc_get
+       || options.sct_erc_set
+  );
+
+  // Exit if no further options specified
+  if (!(   options.drive_info || need_smart_support
+        || need_smart_logdir  || need_gp_logdir
+        || need_sct_support                        )) {
+    if (powername)
+      pout("Device is in %s mode\n", powername);
+    else
+      pout("ATA device successfully opened\n\n"
+           "Use 'smartctl -a' (or '-x') to print SMART (and more) information\n\n");
+    return 0;
+  }
+
   // Start by getting Drive ID information.  We need this, to know if SMART is supported.
+  int returnval = 0;
   ata_identify_device drive; memset(&drive, 0, sizeof(drive));
   int retid = ataReadHDIdentity(device,&drive);
   if (retid < 0) {
@@ -2146,30 +2184,12 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
     print_off();
   }
 
-  // SMART and GP log directories needed?
-  bool need_smart_logdir = (
-          options.smart_logdir
-          // If GP Log is supported use smart log directory for
-          // error and selftest log support check.
-       || (   isGeneralPurposeLoggingCapable(&drive)
-           && (   options.smart_error_log || options.smart_selftest_log
-               || options.retry_error_log || options.retry_selftest_log))
-  );
-
-  bool need_gp_logdir  = (
-          options.gp_logdir
-       || options.smart_ext_error_log
-       || options.smart_ext_selftest_log
-       || options.sataphy
-  );
-
-  unsigned i;
-  for (i = 0; i < options.log_requests.size(); i++) {
-    if (options.log_requests[i].gpl)
-      need_gp_logdir = true;
-    else
-      need_smart_logdir = true;
-  }
+  // If GP Log is supported use smart log directory for
+  // error and selftest log support check.
+  if (   isGeneralPurposeLoggingCapable(&drive)
+      && (   options.smart_error_log || options.smart_selftest_log
+          || options.retry_error_log || options.retry_selftest_log))
+    need_smart_logdir = true;
 
   ata_smart_log_directory smartlogdir_buf, gplogdir_buf;
   const ata_smart_log_directory * smartlogdir = 0, * gplogdir = 0;
@@ -2374,8 +2394,7 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
 
   // SCT commands
   bool sct_ok = false;
-  if (   options.sct_temp_sts || options.sct_temp_hist || options.sct_temp_int
-      || options.sct_erc_get  || options.sct_erc_set                          ) {
+  if (need_sct_support) {
     if (!isSCTCapable(&drive)) {
       pout("Warning: device does not support SCT Commands\n");
       failuretest(OPTIONAL_CMD, returnval|=FAILSMART);

scsiprint.cpp
@@ -1570,22 +1570,27 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
     int returnval = 0;
     int res, durationSec;
 
+    bool any_output = options.drive_info;
+
     res = scsiGetDriveInfo(device, &peripheral_type, options.drive_info);
     if (res) {
         if (2 == res)
             return 0;
         else
             failuretest(MANDATORY_CMD, returnval |= FAILID);
+	any_output = true;
     }
 
     if (options.smart_enable) {
         if (scsiSmartEnable(device))
             failuretest(MANDATORY_CMD, returnval |= FAILSMART);
+	any_output = true;
     }
 
     if (options.smart_disable) {
         if (scsiSmartDisable(device))
             failuretest(MANDATORY_CMD,returnval |= FAILSMART);
+	any_output = true;
     }
     
     if (options.smart_auto_save_enable) {
@@ -1593,6 +1598,7 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
         pout("Enable autosave (clear GLTSD bit) failed\n");
         failuretest(OPTIONAL_CMD,returnval |= FAILSMART);
       }
+      any_output = true;
     }
     
     if (options.smart_auto_save_disable) {
@@ -1600,6 +1606,7 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
         pout("Disable autosave (set GLTSD bit) failed\n");
         failuretest(OPTIONAL_CMD,returnval |= FAILSMART);
       }
+      any_output = true;
     }
     
     if (options.smart_check_status) {
@@ -1623,6 +1630,7 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
                     returnval |= FAILSMART;
             }
         }
+        any_output = true;
     }   
     if (options.smart_vendor_attrib) {
         if (! checkedSupportedLogPages)
@@ -1641,6 +1649,7 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
             if (gSeagateFactoryLPage)
                 scsiPrintSeagateFactoryLPage(device);
         }
+        any_output = true;
     }
     if (options.smart_error_log) {
         if (! checkedSupportedLogPages)
@@ -1649,6 +1658,7 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
         if (1 == scsiFetchControlGLTSD(device, modese_len, 1))
             pout("\n[GLTSD (Global Logging Target Save Disable) set. "
                  "Enable Save with '-S on']\n");
+        any_output = true;
     }
     if (options.smart_selftest_log) {
         if (! checkedSupportedLogPages)
@@ -1662,6 +1672,7 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
         }
         if (0 != res)
             failuretest(OPTIONAL_CMD, returnval|=res);
+        any_output = true;
     }
     if (options.smart_background_log) {
         if (! checkedSupportedLogPages)
@@ -1675,22 +1686,26 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
         }
         if (0 != res)
             failuretest(OPTIONAL_CMD, returnval|=res);
+        any_output = true;
     }
     if (options.smart_default_selftest) {
         if (scsiSmartDefaultSelfTest(device))
             return returnval | FAILSMART;
         pout("Default Self Test Successful\n");
+        any_output = true;
     }
     if (options.smart_short_cap_selftest) {
         if (scsiSmartShortCapSelfTest(device))
             return returnval | FAILSMART;
         pout("Short Foreground Self Test Successful\n");
+        any_output = true;
     }
     if (options.smart_short_selftest) {
         if (scsiSmartShortSelfTest(device))
             return returnval | FAILSMART;
         pout("Short Background Self Test has begun\n");
         pout("Use smartctl -X to abort test\n");
+        any_output = true;
     }
     if (options.smart_extend_selftest) {
         if (scsiSmartExtendSelfTest(device))
@@ -1706,6 +1721,7 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
             pout("Estimated completion time: %s\n", ctime(&t));
         }
         pout("Use smartctl -X to abort test\n");        
+        any_output = true;
     }
     if (options.smart_extend_cap_selftest) {
         if (scsiSmartExtendCapSelfTest(device))
@@ -1716,10 +1732,17 @@ int scsiPrintMain(scsi_device * device, const scsi_print_options & options)
         if (scsiSmartSelfTestAbort(device))
             return returnval | FAILSMART;
         pout("Self Test returned without error\n");
+        any_output = true;
     }           
     if (options.sasphy) {
         if (scsiPrintSasPhy(device, options.sasphy_reset))
             return returnval | FAILSMART;
+        any_output = true;
     }           
+
+    if (!any_output)
+      pout("SCSI device successfully opened\n\n"
+           "Use 'smartctl -a' (or '-x') to print SMART (and more) information\n\n");
+
     return returnval;
 }

smartctl.8.in
@@ -543,7 +543,14 @@ behaviour. This is does not work for SCSI devices yet.
 [ATA only] Specifies if \fBsmartctl\fP should exit before performing any
 checks when the device is in a low\-power mode. It may be used to prevent
 a disk from being spun\-up by \fBsmartctl\fP. The power mode is ignored by
-default. The allowed values of POWERMODE are:
+default.  A nonzero exit status is returned if the device is in one of the
+specified low\-power modes (see RETURN VALUES below).
+
+Note: If this option is used it may also be necessary to specify the device
+type with the \'-d\' option.  Otherwise the device may spin up due to
+commands issued during device type autodetection.
+
+The valid arguments to this option are:
 
 .I never
 \- check the device always, but print the power mode if \'\-i\' is
@@ -1670,7 +1677,7 @@ typically an ATA (IDE) or SATA disk in Linux.
 .nf
 .B smartctl \-a /dev/sdb
 .fi
-Print a large amount of SMART information for drive /dev/sda . This may
+Print a large amount of SMART information for drive /dev/sdb . This may
 be a SCSI disk or an ATA (SATA) disk.
 .PP
 .nf
@@ -1802,7 +1809,8 @@ may also be returned for SCSI disks.
 Command line did not parse.
 .TP
 .B Bit 1:
-Device open failed, or device did not return an IDENTIFY DEVICE structure. 
+Device open failed, device did not return an IDENTIFY DEVICE structure,
+or device is in a low-power mode (see \'\-n\' option above).
 .TP
 .B Bit 2:
 Some SMART command to the disk failed, or there was a checksum error

commit 05f5d3bc708b4b4de77d2b9ef996c837965f62e9

Fix 'smartctl -P show'.  Regression from r3249.
git-svn-id: https://smartmontools.svn.sourceforge.net/svnroot/smartmontools/trunk/smartmontools@3583 4ea69e1a-61f1-4043-bf83-b5c94c648137

ataprint.cpp
@@ -13,8 +13,7 @@
  * any later version.
  *
  * You should have received a copy of the GNU General Public License
- * (for example COPYING); if not, write to the Free
- * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * (for example COPYING); If not, see <http://www.gnu.org/licenses/>.
  *
  * This code was originally developed as a Senior Thesis by Michael Cornwell
  * at the Concurrent Systems Laboratory (now part of the Storage Systems
@@ -2230,9 +2229,10 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
   );
 
   // Exit if no further options specified
-  if (!(   options.drive_info || need_smart_support
-        || need_smart_logdir  || need_gp_logdir
-        || need_sct_support || options.get_set_used)) {
+  if (!(   options.drive_info || options.show_presets
+        || need_smart_support || need_smart_logdir
+        || need_gp_logdir     || need_sct_support
+        || options.get_set_used                      )) {
     if (powername)
       pout("Device is in %s mode\n", powername);
     else