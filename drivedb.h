mirror/smartmontools

commit 5f6b42b64429a1d1ce4bb4a86b705096dce77ae2

drivedb.h: fixed SanDisk X210 regular expression (#329)
git-svn-id: https://svn.code.sf.net/p/smartmontools/code/trunk/smartmontools@3988 4ea69e1a-61f1-4043-bf83-b5c94c648137

drivedb.h
@@ -976,7 +976,7 @@ const drive_settings builtin_knowndrives[] = {
   },
   { "Marvell based SanDisk SSDs",
     "SanDisk SD5SG2[0-9]*G1052E|" // X100 (88SS9174), tested with SanDisk SD5SG2256G1052E/10.04.01
-    "SanDisk SD6SB[12]M[0-9]*G1022I|" // X110/X210 (88SS9175), tested with SanDisk SD6SB1M064G1022I/X231600,
+    "SanDisk SD6SB[12]M[0-9]*G(1022I|)|" // X110/X210 (88SS9175), tested with SanDisk SD6SB1M064G1022I/X231600,
       // SanDisk SD6SB1M256G1022I/X231600, SanDisk SD6SB2M512G1022I/X210400
     "SanDisk SDSSDHP[0-9]*G|" // Ultra Plus (88SS9175), tested with SanDisk SDSSDHP128G/X23[01]6RL
     "SanDisk SDSSDXP[0-9]*G", // Extreme II (88SS9187), tested with SanDisk SDSSDXP480G/R1311

commit 0427586e6435a4ef7776d65269ed798500e95d4b

drivedb.h: Fix regex syntax error (regression from r3988).
git-svn-id: https://svn.code.sf.net/p/smartmontools/code/trunk/smartmontools@3989 4ea69e1a-61f1-4043-bf83-b5c94c648137

drivedb.h
@@ -976,7 +976,7 @@ const drive_settings builtin_knowndrives[] = {
   },
   { "Marvell based SanDisk SSDs",
     "SanDisk SD5SG2[0-9]*G1052E|" // X100 (88SS9174), tested with SanDisk SD5SG2256G1052E/10.04.01
-    "SanDisk SD6SB[12]M[0-9]*G(1022I|)|" // X110/X210 (88SS9175), tested with SanDisk SD6SB1M064G1022I/X231600,
+    "SanDisk SD6SB[12]M[0-9]*G(1022I)?|" // X110/X210 (88SS9175), tested with SanDisk SD6SB1M064G1022I/X231600,
       // SanDisk SD6SB1M256G1022I/X231600, SanDisk SD6SB2M512G1022I/X210400
     "SanDisk SDSSDHP[0-9]*G|" // Ultra Plus (88SS9175), tested with SanDisk SDSSDHP128G/X23[01]6RL
     "SanDisk SDSSDXP[0-9]*G", // Extreme II (88SS9187), tested with SanDisk SDSSDXP480G/R1311